ae94b8aadecb0270166124351c2c5d582201c871
Ken Bannister <kb2ma@runbox.com>
2016-12-08 13:31:49 -0500

    riot2gcoaptest improvements
    * Specify gcoap example directory as a parameter.
    * Comment updates for standard usage.

--- expect/riot2gcoaptest.py
+++ expect/riot2gcoaptest.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 '''Creates RIOT example app, and uses it to GET /ver resource from gcoaptest
-server. Also creates fixed IP address for use of example app as server.
+server. Also creates fixed IP address (fe80::bbbb:2/64) for use of example app
+as server.
 
 Before running, may need to manually create TAP interface and fixed IP address
 for gcoaptest server. See example below.
@@ -12,14 +13,16 @@ Options:
 -d <secs>  -- Server built-in response delay, in seconds
 -r <count> -- Number of times to repeat query, with a 1 second wait between
               response and next request.
+-x <dir>   -- Directory in which to execute the script; must be location of
+              RIOT gcoap example app.
 
 Example:
 
 $ sudo ip tuntap add tap0 mode tap user kbee
 $ sudo ip link set tap0 up
-$ sudo ip address add fe80::aaaa:1/64 dev tap0
+$ sudo ip address add fe80::bbbb:1/64 dev tap0
 
-$ ./riot2gcoaptest.py -a fe80::aaaa:1 -d 1 -r 50
+$ ./riot2gcoaptest.py -a fe80::bbbb:1 -d 1 -r 50 -x /home/kbee/dev/riot/repo/examples/gcoap
 '''
 from __future__ import print_function
 import time
@@ -34,7 +37,7 @@ def main(addr, serverDelay, repeatCount):
 
     ifType = 'tap' if addr[:4] == 'fe80' else 'tun'
     if ifType == 'tap':
-        child.sendline('ifconfig 6 add unicast fe80::aaaa:2/64')
+        child.sendline('ifconfig 6 add unicast fe80::bbbb:2/64')
         child.expect('success:')
     
     child.sendline('coap post {0} 5683 /cf/delay {1}'.format(addr, serverDelay))
@@ -64,15 +67,17 @@ if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option('-a', type='string', dest='addr')
     parser.add_option('-d', type='int', dest='serverDelay', default=0)
+    parser.add_option('-x', type='string', dest='execDir', default='')
     parser.add_option('-r', type='int', dest='repeatCount', default=1)
 
     (options, args) = parser.parse_args()
 
     # Must run RIOT client from gcoap example directory
-    curdir = os.getcwd()
-    dir    = '/home/kbee/dev/riot/repo/examples/gcoap'
-    os.chdir(dir)
+    if options.execDir:
+        curdir = os.getcwd()
+        os.chdir(options.execDir)
     try:
         main(options.addr, options.serverDelay, options.repeatCount)
     finally:
-        os.chdir(curdir)
+        if options.execDir:
+            os.chdir(curdir) 
