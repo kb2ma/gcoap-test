cba1b5b2e57bffc1e0af8c7b3df40dca819faad5
Ken Bannister <kb2ma@runbox.com>
2016-12-14 05:43:24 -0500

    Add 'toobig' test, where request size exceeds gcoap input buffer.

--- expect/libcoap2riot.py
+++ expect/libcoap2riot.py
@@ -8,18 +8,32 @@ Options:
 -a <addr>  -- Address of server
 -r <count> -- Number of times to repeat query, with a 3 second wait between
               response and next request.
+-t <test> --- Name of test to run. Options:
+                repeat-get -- GET request for /cli/stats
+                toobig -- POSTS to a bogus resource (/abcd)
 
 Example:
 
+# Assumes gcoap server exists.
 # Uses address for fixed tap address created by riot2gcoaptest.py
-$ PATH=${PATH}:/home/kbee/dev/libcoap/repo/examples ./libcoap2riot.py -a fe80::bbbb:2 -r 30
+$ PATH=${PATH}:/home/kbee/dev/libcoap/repo/examples ./libcoap2riot.py -a fe80::bbbb:2 -t repeat-get -r 30
 '''
 from __future__ import print_function
 import time
 import re
 import pexpect
 
-def main(addr, repeatCount):
+def main(addr, testName, repeatCount):
+    if testName == 'repeat-get':
+        runRepeatGet(addr, repeatCount)
+    elif testName == 'toobig':
+        runToobig(addr)
+    else:
+        print('Unexpected test name: {0}'.format(testName))
+
+def runRepeatGet(addr, repeatCount):
+    '''Repeats a simple GET request
+    '''
     print('Test: libcoap client GET /cli/stats from RIOT gcoap server')
     addrSuffix = '%tap0' if addr[:4] == 'fe80' else ''
     
@@ -28,11 +42,24 @@ def main(addr, repeatCount):
         cmdText = 'coap-client -N -m get -U -T 5a coap://[{0}{1}]/cli/stats'
         child   = pexpect.spawn(cmdText.format(addr, addrSuffix))
         pattern = '(v.*\n)(\d+\r\n)'
-        child.expect(pattern)
+        child.expect(pattern, timeout=5)
         # Rerun regex to extract and print second group, the response payload.
         match = re.search(pattern, child.after)
         print('Success: {0}'.format(match.group(2)))
+        child.close()
 
+def runToobig(addr):
+    '''POSTs a request that exceeds the max input size in gcoap.
+    '''
+    print('Test: libcoap client POST large payload to RIOT gcoap server')
+    addrSuffix = '%tap0' if addr[:4] == 'fe80' else ''
+    
+    cmdText = 'coap-client -N -m post -U -T 5a coap://[{0}{1}]/abcd -f toobig.txt'
+    child   = pexpect.spawn(cmdText.format(addr, addrSuffix))
+    child.expect(pexpect.TIMEOUT, timeout=5)
+    print('Success: <timeout>'.format(child.after))
+    child.close()
+    
 if __name__ == "__main__":
     from optparse import OptionParser
 
@@ -40,7 +67,8 @@ if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option('-a', type='string', dest='addr')
     parser.add_option('-r', type='int', dest='repeatCount', default=1)
+    parser.add_option('-t', type='string', dest='testName')
 
     (options, args) = parser.parse_args()
 
-    main(options.addr, options.repeatCount)
+    main(options.addr, options.testName, options.repeatCount)
